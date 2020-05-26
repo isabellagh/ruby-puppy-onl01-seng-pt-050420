c53ab5481fe92670b8a43dda2f181a473ea78012
isabella g henriques <isabellagh88@gmail.com>
2020-05-26 13:07:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,29 +1,24 @@
 # Add your code here
 
-class Dog 
-  
+class Dog
+
   attr_accessor :name
+
   @@all = []
-  
+
   def initialize(name)
-    @name = name 
-   
-    
-    
-  end
-  
-  def self.all 
+    @name = name
     @@all << self
-  end 
-  
-  def self.clear_all
-    @@all.clear 
-  end 
-  
-  def self.print_all
+  end
+
+  def self.all
     @@all.each do |dog|
       puts dog.name
     end
-  end 
-  
-end 
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
